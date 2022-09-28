8afb60db08b08d1f6c70a84dd5ced205dffc3be2
Alex WOO <alex23woo@gmail.com>
2022-09-28 23:29:02 +0800

    updates

--- task1.tf
+++ task1.tf
@@ -48,12 +48,8 @@ resource "aws_dynamodb_table" "terraform_locks" {
 
 terraform {
   backend "s3" {
-    bucket = "terraform-up-and-running-state-collection"
-    key = "global/s3/terraform.tfstate"
-    region = "us-east-2"
-
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt = true
+    #key = "global/s3/terraform.tfstate"
+    key            = "workspaces-example/terraform.tfstate"
   }
 }
 
